581eaa275f18ffa6d29edf8cb95052c9ae73541d
yanalufthansa <jnavoronkova@gmail.com>
2025-12-09 19:43:51 +0300

    Add files via upload

---
+++ Задание 6 demoQA/pages/deno_qa_page.py
@@ -0,0 +1,14 @@
+from components.WebElement import WebElement
+from pages.base_page import BasePage
+
+
+class DemoQAPage(BasePage):
+
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/'
+        super().__init__(driver, self.base_url)
+
+        self.icon = WebElement(driver, '#app > header > a')
+        self.btn_elements = WebElement(driver, "#app > div > div > div.home-body > div > div:nth-child(1)")
+        self.footer_text = WebElement(driver, '#app > footer >span')
+        self.h5 = WebElement(driver, "div > div.card-body > h5")
